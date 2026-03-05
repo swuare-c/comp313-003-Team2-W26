4e3858d236e99f912ed59192ea3210ffa9121a4c
Christian Khan <christiankhan100@gmail.com>
2026-03-05 04:45:06 -0500

    Add Firebase config (Initialization and Firestore)

---
+++ backend/config/firebase.js
@@ -0,0 +1,11 @@
+import { admin } from 'firebase-admin';
+import { getFirestore } from 'firebase/firestore';
+import ServiceAccount from '../serviceAccountKey.json';
+
+app = initializeApp({
+    credential: admin.credential.cert(ServiceAccount)
+});
+
+const db = getFirestore(app)
+
+export {app, db}
